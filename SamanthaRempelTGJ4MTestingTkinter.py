794d1f7fc0cc54dc01b94d4ca544fd768ea0e044
SamanthaRempel <samantha.rempel99@gmail.com>
2017-01-16 11:06:07 -0500

    Add files via upload

---
+++ SamanthaRempelTGJ4MTestingTkinter.py
@@ -0,0 +1,47 @@
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+#LABELS AND IMAGES
+'''
+Variables to keep the label maker from getting cluttered
+'''
+root = Tk()
+varOne = StringVar()
+
+#There are a lot of parameters you can put into a label, link on Final Project Google Doc
+labelOne = Label(root, textvariable = varOne, relief = RAISED, font=("Helvetica", 16), fg = "Purple" )
+
+#This sets the string variable to text
+varOne.set("Sam")
+'''
+.pack() is an organizer for labels within the window, it fits them in neatly
+'''
+labelOne.pack()
+
+
+image = Image.open("Flowers1.jpg") #This opens the image using the PIL Image library
+photo = ImageTk.PhotoImage(image) #Uses a function in the Image library ImageTk to make it compatible with Tkinter
+
+
+#There are a lot of parameters you can put into a label, link on Final Project Google Doc
+labelTwo = Label(root, image = photo, relief = RAISED)
+
+labelTwo.image = photo
+
+'''
+.pack() is an organizer for labels within the window, it fits them in neatly
+'''
+labelTwo.pack()
+
+
+'''
+mainloop() is like an infinite loop that runs the program until the user closes the window
+'''
+root.mainloop()
+
+
+#BUTTONS
+
+buttonOne = Button(root, activebackground = red)
