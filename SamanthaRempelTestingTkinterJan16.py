c51cd73991f2de002523ca76b7bc81f3b31ef82b
SamanthaRempel <samantha.rempel99@gmail.com>
2017-01-18 11:19:57 -0500

    Updated on January 18, 2017
    The image will convert to Black and White, but it won't go back to colour.

--- SamanthaRempelTestingTkinterJan16.py
+++ SamanthaRempelTestingTkinterJan16.py
@@ -1,60 +1,39 @@
 import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
+countBW = 0
+root = Tk()
 
 photoShopImage = Image.open("Flowers1.jpg")
 
-# LABELS AND IMAGES
-'''
-Variables to keep the label maker from getting cluttered
-'''
-root = Tk()
-varOne = StringVar()
+pic = Image.open("Flowers1.jpg")  # This opens the image using the PIL Image library
+photo = ImageTk.PhotoImage(pic)  # Uses a function in the Image library ImageTk to make it compatible with Tkinter
 
-# There are a lot of parameters you can put into a label, link on Final Project Google Doc
-labelOne = Label(root, textvariable=varOne, relief=RAISED, font=("Helvetica", 16), fg="Purple")
+label = Label(root, image = photo)
+label.image = photo
+label.place(x = 100, y = 100)
+label.pack()
 
-# This sets the string variable to text
-varOne.set("Sam")
-
-'''
-.pack() is an organizer for labels within the window, it fits them in neatly
-'''
-labelOne.pack()
-
-
-image = Image.open("Flowers1.jpg")  # This opens the image using the PIL Image library
-photo = ImageTk.PhotoImage(image)  # Uses a function in the Image library ImageTk to make it compatible with Tkinter
-
-image2 = Image.open("Flowers2.jpg")
-photo2 = ImageTk.PhotoImage(image2)
-
-# There are a lot of parameters you can put into a label, link on Final Project Google Doc
-
-labelTwo = Label(root, image=photo, relief=RAISED)
-labelTwo.image = photo
-
-'''
-.pack() is an organizer for labels within the window, it fits them in neatly
-'''
-labelTwo.pack()
+def blackAndWhite():
+    label.pack_forget()
+    if countBW % 2 == 0:
+        photoShopImageEdit = photoShopImage.convert("L")
+        photo = ImageTk.PhotoImage(photoShopImageEdit)
+        label.configure(image=photo)
+        label.image = photo
+        label.place(x=100, y=100)
+        label.pack()
+    else:
+        photo = ImageTk.PhotoImage(pic)
+        label.image = photo
+        label.place(x=100, y=100)
+        label.pack()
 
 # BUTTONS http://effbot.org/tkinterbook/button.htm
-def blackAndWhite():
-    labelTwo.pack_forget()
-    photoShopImageEdit = photoShopImage.convert("L")
-    photo2 = ImageTk.PhotoImage(
-    photoShopImageEdit)  # Uses a function in the Image library ImageTk to make it compatible with Tkinter
-    labelTwo.image = photo2
-    labelTwo.pack()
-    mainloop()
 
+buttonOne = Button(root, activebackground='red', command=blackAndWhite, text="Black and White")
 
-buttonOne = Button(root, activebackground='red', command=blackAndWhite, text="SAM")
+buttonOne.place(x = 500, y = 500)
 buttonOne.pack(fill=BOTH, expand=1)
 
-
-'''
-mainloop() is like an infinite loop that runs the program until the user closes the window
-'''
 mainloop()
